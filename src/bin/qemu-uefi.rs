e20c6f4337469a7460a9f0ae8da6be21a4f2fabc
altacountbabi <altacountbabi@users.noreply.github.com>
2024-04-08 14:10:47 +0300

    Build script and a bit of color on the screen

---
+++ src/bin/qemu-uefi.rs
@@ -0,0 +1,15 @@
+use std::{
+    env,
+    process::{self, Command},
+};
+
+fn main() {
+    let mut qemu = Command::new("qemu-system-x86_64");
+    qemu.arg("-m");
+    qemu.arg("1G");
+    qemu.arg("-drive");
+    qemu.arg(format!("format=raw,file={}", env!("UEFI_IMAGE")));
+    qemu.arg("-bios").arg(ovmf_prebuilt::ovmf_pure_efi());
+    let exit_status = qemu.status().unwrap();
+    process::exit(exit_status.code().unwrap_or(-1));
+}
