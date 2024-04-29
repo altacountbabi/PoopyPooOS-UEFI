ff8fcbc8cb2720d8ae5c648d796b7fce9a4ba97d
altacountbabi <radustefan379@gmail.com>
2024-04-29 23:24:16 +0300

    Added semicolon and testing image offsetting

--- kernel/src/image/png.rs
+++ kernel/src/image/png.rs
@@ -16,27 +16,23 @@ pub fn decode_png(raw_data: Vec<u8>) -> PngData {
 
     for chunk in raw_pixels.chunks_exact(4) {
         let alpha = alpha_blend(Color::new(chunk[0], chunk[1], chunk[2]), Color::new(0, 0, 0), chunk[3]);
-        pixels.push(alpha)
+        pixels.push(alpha);
     }
 
     PngData { pixels, header: png_data.0 }
 }
 
 fn alpha_blend(source_color: Color, dest_color: Color, alpha: u8) -> Color {
-    // Normalize alpha to range [0, 1]
     let alpha_normalized = f32::from(alpha) / 255.0;
 
-    // Calculate the blended color components
     let result_red = (f32::from(source_color.red) * alpha_normalized) + (f32::from(dest_color.red) * (1.0 - alpha_normalized));
     let result_green = (f32::from(source_color.green) * alpha_normalized) + (f32::from(dest_color.green) * (1.0 - alpha_normalized));
     let result_blue = (f32::from(source_color.blue) * alpha_normalized) + (f32::from(dest_color.blue) * (1.0 - alpha_normalized));
 
-    // Round the color components to integers
     let result_red = round_f32(result_red) as u8;
     let result_green = round_f32(result_green) as u8;
     let result_blue = round_f32(result_blue) as u8;
 
-    // Return the blended color
     Color::new(result_red, result_green, result_blue)
 }
 
