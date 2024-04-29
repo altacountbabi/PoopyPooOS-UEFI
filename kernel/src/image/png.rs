0c70f41ecd72d4c6b1b67eb8ac6b6ce16ba1f475
altacountbabi <altacountbabi@users.noreply.github.com>
2024-04-29 23:05:20 +0300

    Added alpha support

--- kernel/src/image/png.rs
+++ kernel/src/image/png.rs
@@ -11,12 +11,39 @@ pub struct PngData {
 pub fn decode_png(raw_data: Vec<u8>) -> PngData {
     let png_data = png_decoder::decode(raw_data.as_slice()).unwrap();
 
-    let pixels = png_data.1;
-    let mut blt_pixels: Vec<Color> = Vec::new();
+    let raw_pixels = png_data.1;
+    let mut pixels: Vec<Color> = Vec::new();
 
-    for chunk in pixels.chunks_exact(4) {
-        blt_pixels.push(Color::new(chunk[0], chunk[1], chunk[2]));
+    for chunk in raw_pixels.chunks_exact(4) {
+        let alpha = alpha_blend(Color::new(chunk[0], chunk[1], chunk[2]), Color::new(0, 0, 0), chunk[3]);
+        pixels.push(alpha)
     }
 
-    PngData { pixels: blt_pixels, header: png_data.0 }
+    PngData { pixels, header: png_data.0 }
+}
+
+fn alpha_blend(source_color: Color, dest_color: Color, alpha: u8) -> Color {
+    // Normalize alpha to range [0, 1]
+    let alpha_normalized = f32::from(alpha) / 255.0;
+
+    // Calculate the blended color components
+    let result_red = (f32::from(source_color.red) * alpha_normalized) + (f32::from(dest_color.red) * (1.0 - alpha_normalized));
+    let result_green = (f32::from(source_color.green) * alpha_normalized) + (f32::from(dest_color.green) * (1.0 - alpha_normalized));
+    let result_blue = (f32::from(source_color.blue) * alpha_normalized) + (f32::from(dest_color.blue) * (1.0 - alpha_normalized));
+
+    // Round the color components to integers
+    let result_red = round_f32(result_red) as u8;
+    let result_green = round_f32(result_green) as u8;
+    let result_blue = round_f32(result_blue) as u8;
+
+    // Return the blended color
+    Color::new(result_red, result_green, result_blue)
+}
+
+fn round_f32(value: f32) -> i32 {
+    if value >= 0.0 {
+        (value + 0.5) as i32
+    } else {
+        (value - 0.5) as i32
+    }
 }
